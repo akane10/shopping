d012256e88883ae4f53621008c696fd896a9bf12
yapie <targaryen.akane@gmail.com>
2019-06-23 01:05:17 +0700

    commit

--- shopping.hs
+++ shopping.hs
@@ -11,12 +11,33 @@ shoppingList =
   , "Cereal"
   ]
 
+showList' :: [String] -> String
+showList' list = ("there are "
+                  ++ (show $ length list)
+                  ++ " items on shopping list."
+                  ++ " and the list is : "
+                  ++ joinWithCommas' list)
+
+add :: String -> [String]
+add item = item : shoppingList
+
 main :: IO()
-main = putStrLn ("there are "
-                ++ (show $ length shoppingList)
-                ++ " items on shopping list."
-                ++ " and the list is : "
-                ++ joinWithCommas' shoppingList)
+main = do 
+   putStrLn $ showList' shoppingList
+   putStrLn "type 'add' to a new item"
+   putStrLn "type 'exit' to exit"
+   command <- getLine
+   case command of
+     "add" -> do
+        putStrLn "what is it?"
+        item <- getLine
+        let added = add item
+        putStrLn "your list is now"
+        putStrLn $ showList' added
+     "exit" ->
+        putStrLn "bye"
+     _ -> 
+       putStrLn "what the heck are typing?"
 
 -- joinWithCommas :: [String] -> String
 -- joinWithCommas [] = ""
